d3435bc6b5d30f1edd1f7a6bab5f6a520849256c
Edwin Suarez <suarezerx@gmail.com>
2024-11-02 14:58:15 -0300

    cretae events

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import style from "./Timer.module.css";
 
-const Timer = ({ time }) => {
+const Timer = ({ minutes, seconds }) => {
   return (
     <div className={style.lineReloj}>
       <div className={style.circle}>
         <img src="./pomodoro.png" alt="pomodoro" width="150px" />
-        <div className={style.boxDigitalReloj}>{time}</div>
+        <div className={style.boxDigitalReloj}>
+          {String(minutes).padStart(2, "0")}:{String(seconds).padStart(2, "0")}
+        </div>
       </div>
     </div>
   );
